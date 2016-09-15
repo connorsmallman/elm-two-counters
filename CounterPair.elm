c554b51bc54ccf85c26011b531e4823991fdff8b
connorsmallman <connor.smallman@bedegaming.com>
2016-09-15 15:35:14 +0100

    Update CounterPair.elm

--- CounterPair.elm
+++ CounterPair.elm
@@ -28,8 +28,10 @@ update : Action -> Model -> Model
 update action model =
   case action of
     Reset -> init 0 0
-    Top act -> {model | topCounter = Counter.update act model.topCounter}
-    Bottom act -> {model | bottomCounter = Counter.update act model.bottomCounter}
+    Top act -> 
+      {model | topCounter = Counter.update act model.topCounter}
+    Bottom act -> 
+      {model | bottomCounter = Counter.update act model.bottomCounter}
 
 
 -- View
